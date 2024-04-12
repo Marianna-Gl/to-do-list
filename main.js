0b251eae6e85c341da709a7d30e7d93dbd1be247
Marianna Galichyan <marianna.galichyan@gmail.com>
2024-04-12 08:42:03 +0400

    new added package

---
+++ main.js
@@ -0,0 +1,21 @@
+@@ -1,11 +1,15 @@
+import './assets/main.css'
+import 'vuetify/styles'
+
+import { createApp } from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import App from './App.vue'
+
+const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+// app.use(BootstrapVue)
+app.mount('#app')
+createApp(App).use(vuetify).mount('#app')
